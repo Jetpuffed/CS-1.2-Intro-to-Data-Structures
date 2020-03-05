80f36cc0d6b4989ac3ef204234e98526615dd99a
Jetpuffed <brandon@jetpuffed.org>
2020-03-05 09:55:38 -0800

    finished final project

--- app.py
+++ app.py
@@ -3,12 +3,17 @@
 
 from flask import Flask
 
+from markov import MarkovChain
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "A randomly generated sentence will go here."
+    markov = MarkovChain(
+        ["one", "fish", "two", "fish", "red", "fish", "blue", "fish"],
+    )
+    return markov.walk(10)
 
 
 if __name__ == "__main__":
